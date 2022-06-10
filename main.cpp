7e0fb23a456faededa51cdc358bd94010fa2fe1f
hoangtukbl <57582122+hoangtukbl@users.noreply.github.com>
2022-06-10 19:25:52 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <time.h>
+#include "algorithm_mode.h"
+#include "comparison_mode.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    string temp1 = argv[1];
+    if(temp1 == "-a")
+        AlgorithmMode( argc ,argv);
+    else if(temp1 == "-c")
+        ComparisonMode(argc,argv);
+    else    
+        cout << "wrong input";
+    return 0;
+}   
