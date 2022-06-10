2e6ca03258f0ec6055d2bf167d4a95d5da899bd4
hoangtukbl <57582122+hoangtukbl@users.noreply.github.com>
2022-06-10 19:25:35 +0700

    Create comparison_mode.h

---
+++ comparison_mode.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "algorithm_mode.h"
+#include "DataGenerator.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+void ComparisonMode(int temp_argc , char* temp_argv[]);
