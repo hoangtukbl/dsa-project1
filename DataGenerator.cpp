27f67e08362a607d3ef52a8dddaf74151e330543
hoangtukbl <haphonglinh.ht@gmail.com>
2022-06-11 14:58:41 +0700

    Update DataGenerator.cpp

--- DataGenerator.cpp
+++ DataGenerator.cpp
@@ -11,7 +11,7 @@ void HoanVi(T &a, T &b)
 //-------------------------------------------------
 
 // Hàm phát sinh mảng dữ liệu ngẫu nhiên
-void GenerateRandomData(int a[], int n)
+void GenerateRandomData(int* a, int n)
 {
 	srand((unsigned int)time(NULL));
 
@@ -22,7 +22,7 @@ void GenerateRandomData(int a[], int n)
 }
 
 // Hàm phát sinh mảng dữ liệu có thứ tự tăng dần
-void GenerateSortedData(int a[], int n)
+void GenerateSortedData(int* a, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -31,7 +31,7 @@ void GenerateSortedData(int a[], int n)
 }
 
 // Hàm phát sinh mảng dữ liệu có thứ tự ngược (giảm dần)
-void GenerateReverseData(int a[], int n)
+void GenerateReverseData(int* a, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -40,7 +40,7 @@ void GenerateReverseData(int a[], int n)
 }
 
 // Hàm phát sinh mảng dữ liệu gần như có thứ tự
-void GenerateNearlySortedData(int a[], int n)
+void GenerateNearlySortedData(int* a, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -55,7 +55,7 @@ void GenerateNearlySortedData(int a[], int n)
 	}
 }
 
-void GenerateData(int a[], int n, int dataType)
+void GenerateData(int* a, int n, int dataType)
 {
 	switch (dataType)
 	{
