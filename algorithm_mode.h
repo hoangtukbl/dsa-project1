1c0fb07b3bf23aede0c7e6d8739fd5e4df7c8762
hoangtukbl <57582122+hoangtukbl@users.noreply.github.com>
2022-06-13 17:01:17 +0700

    Update algorithm_mode.h

--- algorithm_mode.h
+++ algorithm_mode.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <fstream>
 #include "DataGenerator.h"
 #include "comparison_mode.h"
+#include "sort.h"
 using namespace std;
 
 int output_order(string order);
 
-void cout_output(int order);
+
+void cout_comp(int al, int size, int * a);
 
 int input_order(string order);
 
-void cout_input(int order);
+void cout_input(int order,int size , string name);
 
 int al_order(string order);
 
