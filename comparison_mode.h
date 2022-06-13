3034bdb0ea80d396b6b310110baf761ad3e8d0f7
hoangtukbl <57582122+hoangtukbl@users.noreply.github.com>
2022-06-13 17:01:24 +0700

    Update comparison_mode.h

--- comparison_mode.h
+++ comparison_mode.h
@@ -5,10 +5,10 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include "sort.h"
 
 using namespace std;
 
-void make_input_file(int order ,int size,string name);
-
+int* read_input_file(string name);
 void cout_2al(int order1 , int order2);
 void ComparisonMode(int temp_argc , char* temp_argv[]);
