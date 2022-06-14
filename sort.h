3af3090b2adf7a93ce0af5b2d4ce7a8c41c2fafa
hoangtukbl <57582122+hoangtukbl@users.noreply.github.com>
2022-06-14 19:34:39 +0700

    Update sort.h

--- sort.h
+++ sort.h
@@ -22,3 +22,14 @@ void ShakerSort_comp(int* array, int size);
 
 void RadixSort_comp(int* array, int size);
 
+void bubbleSort_comp(int arr[], int n);
+
+void quickSort1_comp(int* arr, int l, int r, int& comp);
+void quickSort_comp(int* arr, int n);
+
+int shellSort_comp(int* arr, int n);
+
+void heapify_comp(int* a, int n, int i, int& comp);
+void heapSort_comp(int* a, int n);
+
+
